68001db87692e6114f79cb4691955c4eb8731942
moshe980 <57193257+moshe980@users.noreply.github.com>
2022-07-13 16:01:48 +0300

    init

--- app/src/main/java/com/giniapps/weatherapplication/networking/NetworkStatusChecker.kt
+++ app/src/main/java/com/giniapps/weatherapplication/networking/NetworkStatusChecker.kt
@@ -1,16 +1,18 @@
 package com.giniapps.weatherapplication.networking
 
+import android.annotation.SuppressLint
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 
 class NetworkStatusChecker(private val connectivityManager: ConnectivityManager?) {
-
+//dd
+    @SuppressLint("MissingPermission")
     fun hasInternetConnection(): Boolean {
         val network = connectivityManager?.activeNetwork ?: return false
 
         val capabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
 
-        //if capabilities is WIFI/CELLULAR/VPN true 
+        //if capabilities is WIFI/CELLULAR/VPN true
         return capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
                 capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
                 capabilities.hasTransport(NetworkCapabilities.TRANSPORT_VPN)
